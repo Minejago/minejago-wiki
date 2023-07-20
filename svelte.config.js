7fe40e2a4435dafa0b966d7904b89a53e839308d
Thomas Glasser <tjglass06@gmail.com>
2023-07-19 22:37:34 -0500

    Attempted to fix deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	],
+	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH,
+		}
 	}
 };
 
