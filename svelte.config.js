3f48db7bd96adc44b3c7cbc95289af2271a5e2aa
Thomas Glasser <tjglass06@gmail.com>
2023-07-19 22:46:51 -0500

    Fixed crash

--- svelte.config.js
+++ svelte.config.js
@@ -8,15 +8,6 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: adapter(),
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH,
-		}
-	}
 };
 
 export default config;
